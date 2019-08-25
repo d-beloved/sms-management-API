11302a69e5725b1685b018e51018894361f98727
d-beloved <morayodeji@gmail.com>
2019-08-25 20:43:11 +0100

    set for deployment

--- src/models/index.js
+++ src/models/index.js
@@ -1,8 +1,14 @@
+/* eslint-disable arrow-body-style */
+/* eslint-disable dot-notation */
+/* eslint-disable arrow-parens */
+/* eslint-disable global-require */
+/* eslint-disable import/no-dynamic-require */
 /* eslint-disable prefer-template */
 /* eslint-disable no-path-concat */
-/* eslint-disable import/no-dynamic-require */
-/* eslint-disable dot-notation */
-/* eslint-disable global-require */
+/* eslint-disable lines-around-directive */
+/* eslint-disable strict */
+'use strict';
+
 require('dotenv').config();
 
 const fs = require('fs');
@@ -24,13 +30,15 @@ if (config.use_env_variable) {
 
 fs
   .readdirSync(__dirname)
-  .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
-  .forEach((file) => {
+  .filter(file => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach(file => {
     const model = sequelize['import'](path.join(__dirname, file));
     db[model.name] = model;
   });
 
-Object.keys(db).forEach((modelName) => {
+Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
